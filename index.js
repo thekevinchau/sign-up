13899a3fb1d16a58f3ff10570add08ede5c4521b
KEVIN CHAU <kevinch@udel.edu>
2023-11-16 17:28:57 -0500

    finished and pushing to prod

---
+++ index.js
@@ -0,0 +1,7 @@
+let password = document.querySelector('#password').value
+let password_val = document.querySelector('#password_val').value
+let button = document.querySelector('#submission')
+
+button.addEventListener('click', () => {
+        alert("Passwords do not match!")
+})
